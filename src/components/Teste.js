3fa03b9e217fdd534b3ba54829ea8db9bfe8e4af
Lucas Siqueira <lalvessiqueira8@gmail.com>
2020-02-17 17:55:02 -0300

    aplicando conceito de imutabilidade

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // import { Container } from './styles';
 
 export default class Teste extends Component {
   state = {
-    tests: ['Teste1', 'Teste2', 'Teste3']
-  }
+    newTest: "",
+    tests: ["Teste1", "Teste2", "Teste3"]
+  };
+  handleInputChange = e => {
+    this.setState({ newTest: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ tests: [...this.state.tests, this.state.newTest], newTest: '' });
+  };
+
   render() {
     console.log(this.state);
-    return <ul>
-      <li>Teste1</li>
-      <li>Teste2</li>
-      <li>Teste3</li>
-    </ul>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTest}</h1>
+        <ul>
+          {this.state.tests.map(test => (
+            <li key={test}>{test}</li>
+          ))}
+        </ul>
+        <input type="text" onChange={this.handleInputChange} />
+        <button type="submit">Enviar</button>
+      </form>
+    );
   }
 }
