4c8317ebaa11e8e741125dd99d09bd46f495cf46
Thierry Santos <thierry.santos@skyone.solutions>
2020-03-25 10:37:36 -0300

    chore(): configure libraries

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+/* eslint-disable react/no-render-return-value */
+/* eslint-disable global-require */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { history } from './services';
+import store from './store';
+import theme from './theme';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+const render = (Component) => {
+	return ReactDOM.render(
+		<Provider store={store}>
+			<Router history={history}>
+				<ThemeProvider theme={theme}>
+					<Component />
+				</ThemeProvider>
+			</Router>
+		</Provider>,
+		document.getElementById('root')
+	);
+};
 
-serviceWorker.unregister();
+render(App);
+
+if (module.hot) {
+	module.hot.accept('./App', () => {
+		const NextApp = require('./App').default;
+		render(NextApp);
+	});
+}
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '~/pages/Login';
+// import { Container } from './styles';
+
+export default function Routes() {
+	return (
+		<Switch>
+			<Route exact path="/">
+				<Login />
+			</Route>
+		</Switch>
+	);
+}
